e214ad20e641f8b47558ff0b6e51eb074889c493
Nikandrova <enikandrova@hiplabs.dev>
2021-12-28 00:05:12 +0300

    imported Jetpack Compose and learned what it is

--- app/src/main/java/com/android/movieoftheday/MainActivity.kt
+++ app/src/main/java/com/android/movieoftheday/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.android.movieoftheday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.ic_baseline_face),
+            contentDescription = "Contact profile picture"
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
