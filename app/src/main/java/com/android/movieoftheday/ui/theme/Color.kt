a4e28b23770cbf6352bb47d87b3e4091721e83a0
Zalyeva <zaliaeva@raketa.im>
2022-01-18 22:44:15 +0300

    added themes

---
+++ app/src/main/java/com/android/movieoftheday/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.android.movieoftheday.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFF695E93)
+val Purple500 = Color(0xFF8155BA)
+val Purple700 = Color(0xFF281C2D)
+val Purple300 = Color(0xFFBEAFC2)
+val Purple100 = Color(0xFFC55FFC)
+
+val Gray500 = Color(0xFF323E42)
